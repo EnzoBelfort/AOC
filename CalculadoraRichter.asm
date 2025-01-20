bc073d5cd488a466800ee31b93e71bf5f330d1ba
EnzoBelfort <belfortenzo@gmail.com>
2025-01-20 17:22:15 -0300

    iniciando calculadora Escala Richter

--- CalculadoraRichter.asm
+++ CalculadoraRichter.asm
@@ -1,18 +1,18 @@
 .data 
 str1: .asciiz "Sistema de simulação de tsunamis e calculadora de escala Richter\n\n"
-str2: .asciiz "Você pode digitar diretamente a magnitude do terremoto na escala Richter (M) para ser usado em um sistema 
-de previsão de tsunamis ou, então, escolher um valor para a amplitude máxima do movimento do solo (A) e um valor para a 
-distância entre o epicentro do terremoto e a estação sismográfica (d) para, primeiramente, calcular a magnitude (M) 
-e depois a previsão.\n\n" 
+str2: .asciiz "Você pode digitar diretamente a magnitude do terremoto na escala Richter (M) para ser usado em um sistema de 
+previsão de tsunamis ou, então, escolher um valor para a amplitude máxima do movimento do solo (A), medida em micrômetros 
+e um valor para a distância entre o epicentro do terremoto e a estação sismográfica (d), medida em quilômetros, para, 
+primeiramente, calcular a magnitude (M) e depois a previsão.\n\n" 
 str3: .asciiz "Menu de opções:\n"
 str4: .asciiz "Digite 1 para informar a amplitude máxima e a distância do epicentro\n"
 str5: .asciiz "Digite 2 para informar a magnitude do terremoto ja na escala Richter\n"
 str6: .asciiz "Digite 3 para sair do simulador\n\n"
 str7: .asciiz "Escolha uma opção do menu: "
 str8: .asciiz "Valor inválido! Digite 1, 2 ou 3 para acessar alguma função do simulador.\n"
-str9: .asciiz "Digite o valor"
-str10: .asciiz "Digite o valor"
-str11: .asciiz "Digite a magnitude do terromoto com uma casa decimal: "
+str9: .asciiz "Digite um inteiro para a amplitude máxima A, em micrômetros (μm): "
+str10: .asciiz "Digite um inteiro para a distância d, em quilômentros (km): "
+str11: .asciiz "Digite a magnitude do terromoto M, com uma casa decimal: "
 newline: .asciiz "\n"
 
 .globl main
@@ -69,10 +69,27 @@ valor_invalido:
     j while                                             #volta para o while 
 
 calculadora_Richter:
+    li $v0, 4
+    la $a0, str9
+    syscall
+
+    li $v0, 5                                           # lendo a amplitude A como um inteiro
+    syscall
+    move $t1, $v0                                       #t1 = Amplitude maxima em μm
+
+    li $v0, 4
+    la $a0, str10
+    syscall
+
+    li $v0, 5                                           # lendo a distancia d como um inteiro
+    syscall
+    move $t2, $v0                                       #t2 = distancia em km
 
 
+    #transformar a distancia d em Ao por meio de uma tabela de conversão por intervalos  
+    #calcular log(A) e log(Ao) e, depois, M = log(A) - log(Ao)
 
-
+    
 
 
 
